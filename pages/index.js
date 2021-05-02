5e78c76c869fdf9e72ee09e0efd8f3664f085162
msnegurski <mishasneg@gmail.com>
2021-05-02 07:08:18 +0200

    Add background

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,32 @@ export default function Home() {
     const [endDate, setEndDate] = useState(new Date().setMonth(startDate.getMonth() + 1))
 
     return (
-        <div className="min-h-screen flex items-center max-w-2xl mx-auto justify-center space-x-4">
-            <div className="relative">
-                <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    selectsStart
-                    startDate={startDate}
-                    endDate={endDate}
-                    minDate={new Date()}
-                    nextMonthButtonLabel=">"
-                    previousMonthButtonLabel="<"
-                />
-            </div>
-            <div className="relative">
-                <DatePicker
-                    selected={endDate}
-                    onChange={(date) => setEndDate(date)}
-                    selectsEnd
-                    startDate={startDate}
-                    endDate={endDate}
-                    minDate={startDate}
-                    nextMonthButtonLabel=">"
-                    previousMonthButtonLabel="<"
-                />
+        <div className="bg-gray-100">
+            <div className="min-h-screen flex items-center max-w-2xl mx-auto justify-center space-x-4">
+                <div className="relative">
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        selectsStart
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={new Date()}
+                        nextMonthButtonLabel=">"
+                        previousMonthButtonLabel="<"
+                    />
+                </div>
+                <div className="relative">
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        selectsEnd
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={startDate}
+                        nextMonthButtonLabel=">"
+                        previousMonthButtonLabel="<"
+                    />
+                </div>
             </div>
         </div>
     )
