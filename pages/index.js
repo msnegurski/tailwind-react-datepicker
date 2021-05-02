95e58ac91c1806191e3c826ea15bd5ab02c23219
msnegurski <mishasneg@gmail.com>
2021-05-02 07:06:41 +0200

    Place datepicker on the page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
+import DatePicker from 'react-datepicker'
+import { useState } from 'react'
+
 export default function Home() {
-  return (
-    <div>
-    </div>
-  )
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date().setMonth(startDate.getMonth() + 1))
+
+    return (
+        <div className="min-h-screen flex items-center max-w-2xl mx-auto justify-center space-x-4">
+            <div className="relative">
+                <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={new Date()}
+                    nextMonthButtonLabel=">"
+                    previousMonthButtonLabel="<"
+                />
+            </div>
+            <div className="relative">
+                <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    nextMonthButtonLabel=">"
+                    previousMonthButtonLabel="<"
+                />
+            </div>
+        </div>
+    )
 }
