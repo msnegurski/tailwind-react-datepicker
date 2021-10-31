08084550aed27c0d6bda2390ddbd0fe666316fe2
msnegurski <mishasneg@gmail.com>
2021-10-31 06:58:17 +0100

    Add custom components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import DatePicker from 'react-datepicker'
-import { useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { format } from 'date-fns'
 
 export default function Home() {
     const [startDate, setStartDate] = useState(new Date())
@@ -8,7 +10,7 @@ export default function Home() {
     return (
         <div className="bg-gray-100">
             <div className="flex items-center justify-center max-w-2xl min-h-screen mx-auto space-x-4">
-                <div className="relative">
+                <div className="relative w-40">
                     <DatePicker
                         selected={startDate}
                         onChange={(date) => setStartDate(date)}
@@ -19,9 +21,49 @@ export default function Home() {
                         nextMonthButtonLabel=">"
                         previousMonthButtonLabel="<"
                         popperClassName="react-datepicker-left"
+                        customInput={<ButtonInput />}
+                        renderCustomHeader={({
+                            date,
+                            decreaseMonth,
+                            increaseMonth,
+                            prevMonthButtonDisabled,
+                            nextMonthButtonDisabled,
+                        }) => (
+                            <div className="flex items-center justify-between px-2 py-2">
+                                <span className="text-lg text-gray-700">
+                                    {format(date, 'MMMM yyyy')}
+                                </span>
+
+                                <div className="space-x-2">
+                                    <button
+                                        onClick={decreaseMonth}
+                                        disabled={prevMonthButtonDisabled}
+                                        type="button"
+                                        className={`
+                                            ${prevMonthButtonDisabled && 'cursor-not-allowed opacity-50'}
+                                            inline-flex p-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-blue-500
+                                        `}
+                                    >
+                                        <ChevronLeftIcon className="w-5 h-5 text-gray-600" />
+                                    </button>
+
+                                    <button
+                                        onClick={increaseMonth}
+                                        disabled={nextMonthButtonDisabled}
+                                        type="button"
+                                        className={`
+                                            ${nextMonthButtonDisabled && 'cursor-not-allowed opacity-50'}
+                                            inline-flex p-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-blue-500
+                                        `}
+                                    >
+                                        <ChevronRightIcon className="w-5 h-5 text-gray-600" />
+                                    </button>
+                                </div>
+                            </div>
+                        )}
                     />
                 </div>
-                <div className="relative">
+                <div className="relative w-40">
                     <DatePicker
                         selected={endDate}
                         onChange={(date) => setEndDate(date)}
@@ -32,9 +74,60 @@ export default function Home() {
                         nextMonthButtonLabel=">"
                         previousMonthButtonLabel="<"
                         popperClassName="react-datepicker-right"
+                        customInput={<ButtonInput />}
+                        renderCustomHeader={({
+                            date,
+                            decreaseMonth,
+                            increaseMonth,
+                            prevMonthButtonDisabled,
+                            nextMonthButtonDisabled,
+                        }) => (
+                            <div className="flex items-center justify-between px-2 py-2">
+                                <span className="text-lg text-gray-700">
+                                    {format(date, 'MMMM yyyy')}
+                                </span>
+
+                                <div className="space-x-2">
+                                    <button
+                                        onClick={decreaseMonth}
+                                        disabled={prevMonthButtonDisabled}
+                                        type="button"
+                                        className={`
+                                            ${prevMonthButtonDisabled && 'cursor-not-allowed opacity-50'}
+                                            inline-flex p-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-blue-500
+                                        `}
+                                    >
+                                        <ChevronLeftIcon className="w-5 h-5 text-gray-600" />
+                                    </button>
+
+                                    <button
+                                        onClick={increaseMonth}
+                                        disabled={nextMonthButtonDisabled}
+                                        type="button"
+                                        className={`
+                                            ${nextMonthButtonDisabled && 'cursor-not-allowed opacity-50'}
+                                            inline-flex p-1 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-blue-500
+                                        `}
+                                    >
+                                        <ChevronRightIcon className="w-5 h-5 text-gray-600" />
+                                    </button>
+                                </div>
+                            </div>
+                        )}
                     />
                 </div>
             </div>
         </div>
     )
 }
+
+const ButtonInput = forwardRef(({ value, onClick }, ref) => (
+    <button
+        onClick={onClick}
+        ref={ref}
+        type="button"
+        className='inline-flex justify-start w-full px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-blue-500'
+    >
+        {format(new Date(value), 'dd MMMM yyyy')}
+    </button>
+))
